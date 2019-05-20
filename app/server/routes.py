73fef62372c4dc56b7796eb3fa7c042c6b2d1ea6
Axel Wretman <axewr193@student.liu.se>
2019-05-20 13:45:28 +0200

    get_comments probably works now.

--- app/server/routes.py
+++ app/server/routes.py
@@ -46,9 +46,7 @@ def get_comments(postid):
                 plain_response("Given post ID doesn't exist. Requested resource not found."), 404)
         return respond(plain_response("Requested post has no comments."), 404)
     comments_list = serialize_list(comments)
-    print(comments_list)
-    sys.stdout.flush()
-    return respond(comments_list)
+    return respond({'comments': comments_list})
 
 
 @app.route('/post/<postid>')
